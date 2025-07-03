eaf30c8955027a441e9d1e810fa9e88306502f05
Prithvich97 <113849850+Prithvich97@users.noreply.github.com>
2025-07-03 17:21:19 -0400

    Added jenkins config file

---
+++ jenkins-vm/t.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo gpg --dearmor -o /usr/share/keyrings/trivy.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/trivy.list
+
+sudo apt-get update
+sudo apt-get install trivy -y
+
